e6a875276db3737cd6a6988f525726a44bb360ee
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-05-25 14:35:21 +0200

    added coloring functionality

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const container =document.getElementById("container");
-
+const container = document.getElementById("container");
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
 
+    let isMouseDown = false; //tracks whether mouse button is pressed or not
+
+    for (let c = 0; c < rows * cols; c++) {
+        let cell = document.createElement("div");
+        cell.innerText = c + 1;
+        cell.className = "grid-item";
+
+        // Added mousedown and mouseover listeners to each grid item
+        cell.addEventListener('mousedown', function () {
+            isMouseDown = true;
+            this.style.backgroundColor = "black";
+            // Added global listener for mousemove events while button is held down
+
+            window.addEventListener('mousemove', function (e) {
+                if (isMouseDown && e.target.classList.contains('grid-item')) {
+                    e.target.style.backgroundColor = "black";
+                }
+            });
+        });
+
+        // Added mouseup listener to stop coloring cells when button is released
+        window.addEventListener('mouseup', function () {
+            isMouseDown = false;
+        });
+        container.appendChild(cell);
+    }
+}
 makeRows(16, 16);
+
+
+
